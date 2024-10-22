40b16ba3a52cd3bd58b545d166748b99bf8fcff1
jesusparfer27 <jesusparfer@alumnos.cei.es>
2024-10-22 18:43:06 +0200

    commit

--- front/src/pages/HomePage.jsx
+++ front/src/pages/HomePage.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import SeasonVideo from '../assets/home-video-season.mp4';
 import { NavLink } from 'react-router-dom';
 import '../css/pages/homepage.css';
-import imageHome from '../assets/photos/pexels-sebastiaan9977-1311590.jpg'
+import autumnImage from '../assets/autumn-session-home.jpg';
+import winterImage from '../assets/winter-session-home.jpg';
 
 export const HomePage = () => {
     const [offset, setOffset] = useState(0);
@@ -22,38 +23,37 @@ export const HomePage = () => {
     };
 
     useEffect(() => {
-        // Manejar el desplazamiento para el carrusel
         const interval = setInterval(() => {
-            setOffset((prevOffset) => (prevOffset >= 100 ? 0 : prevOffset + 0.5));
-        }, 30); // Ajusta el valor para cambiar la velocidad del desplazamiento
+            setOffset((prevOffset) => (prevOffset >= 100 ? 0 : prevOffset + 100 / productsData.products.length));
+        }, 3000); // Cambia la velocidad del desplazamiento a tu gusto
 
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
-        // Manejar el desplazamiento de la ventana
         const handleScroll = () => {
-            const offsetY = window.scrollY; // Obtener el desplazamiento vertical
-            const newScale = 1 + offsetY / 7000; // Calcular el nuevo valor de escala
-            setScale(newScale); // Actualizar el estado del zoom
+            const offsetY = window.scrollY;
+            const newScale = 1 + (offsetY > 100 ? (offsetY - 100) / 7000 : 0); // Ajusta el divisor para modificar la intensidad del zoom
+            setScale(newScale);
         };
 
-        window.addEventListener('scroll', handleScroll); // Agregar el listener al scroll
+        window.addEventListener('scroll', handleScroll);
 
         return () => {
-            window.removeEventListener('scroll', handleScroll); // Limpiar el listener al desmontar el componente
+            window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
     return (
         <main>
-            <article className='stickyContainer-1'>
             <section className="videoScrollContainer">
                 <div className="videoWrapper">
                     <img
                         className="scrollImage"
-                        style={{ transform: `scale(${scale})`, transition: 'transform 0.1s ease' }} // Aplicar el estilo de zoom
-                        src={imageHome} />
+                        style={{ transform: `scale(${scale})`, transition: 'transform 0.1s ease' }}
+                        src={winterImage}
+                        alt="Imagen de fondo"
+                    />
                 </div>
             </section>
 
@@ -81,78 +81,69 @@ export const HomePage = () => {
                 </div>
             </section>
 
-            <section>
-    <div className="container_videoSession-2">
-        <NavLink to="/video" className="videoLink">
-            <div className="videoContentWrapper">
-                <video
-                    className="videoElement"
-                    autoPlay
-                    muted
-                    loop
-                    playsInline
-                >
-                    <source src={SeasonVideo} type="video/mp4" />
-                    Tu navegador no soporta la reproducción de videos.
-                </video>
-                <div className="textOverlay">
-                    <h2>Explora lo nuevo de la temporada</h2>
-                    <p>Descubre las últimas colecciones que hemos preparado para ti.</p>
-                </div>
-            </div>
-        </NavLink>
-    </div>
-</section>
-</article>
-
-<article className='stickyContainer-2'>
-            <section className="carruselHome">
-    <div className="leftTextContainer">
-        <p>Descubre nuestros productos destacados</p>
-    </div>
-    <div className="rightCarouselContainer">
-        <div className="carousel" style={{ transform: `translateX(-${offset}%)`, transition: 'transform 0.1s linear' }}>
-            {[...productsData.products, ...productsData.products].map((product, index) => (
-                <NavLink
-                    to={`/product/${product.id}`}
-                    key={index}
-                    className="carouselItem"
-                >
-                    <img
-                        src={product.image}
-                        alt={product.name}
-                        className="carouselImage"
-                    />
-                    <p>{product.name}</p>
-                </NavLink>
-            ))}
-        </div>
-    </div>
-</section>
-
-
-            <section>
-                <div className="container_video">
-                    <NavLink to="/video" className="videoLink2">
-                        <video
-                            width="100%"
-                            height="auto"
-                            autoPlay
-                            muted
-                            loop
-                            playsInline
-                        >
-                            <source src={SeasonVideo} type="video/mp4" />
-                            Tu navegador no soporta la reproducción de videos.
-                        </video>
+            <section className="container_videoSession-2">
+                <div>
+                    <NavLink to="/video" className="videoLink">
+                        <div className="videoContentWrapper">
+                            <video
+                                className="videoElement"
+                                autoPlay
+                                muted
+                                loop
+                                playsInline
+                            >
+                                <source src={SeasonVideo} type="video/mp4" />
+                                Tu navegador no soporta la reproducción de videos.
+                            </video>
+                            <div className="textOverlay">
+                                <h2>Explora lo nuevo de la temporada</h2>
+                                <p>Descubre las últimas colecciones que hemos preparado para ti.</p>
+                            </div>
+                        </div>
                     </NavLink>
                 </div>
             </section>
 
-            <section className="newCollections">
+            <section className="carruselHome">
+                <div className="leftTextContainer">
+                    <p>Descubre nuestros productos destacados</p>
+                </div>
+                <div className="rightCarouselContainer">
+                    <div className="carousel" style={{ transform: `translateX(-${offset}%)` }}>
+                        {/* Repetimos los productos para crear un efecto de carrusel infinito */}
+                        {[...productsData.products, ...productsData.products].map((product) => (
+                            <NavLink
+                                to={`/product/${product.id}`}
+                                key={product.id} // Cambia a product.id para evitar claves duplicadas
+                                className="carouselItem" // Aquí solo asignamos una clase específica para el carrusel
+                            >
+                                <img
+                                    src={product.image}
+                                    alt={product.name}
+                                    className="carouselImage"
+                                />
+                                <p>{product.name}</p>
+                            </NavLink>
+                        ))}
+                    </div>
+                </div>
+            </section>
+
+            <section className="container_image_autumn stickyVideo">
+                <div>
+                    <NavLink to="/video" className="videoLink2">
+                        <img className='imageAutumn' 
+                             style={{ transform: `scale(${scale})`, transition: 'transform 0.1s ease' }} 
+                             src={autumnImage} 
+                             alt="Imagen de otoño" />
+                    </NavLink>
+                </div>
+            </section>
+
+            <section className="newCollections lastSection">
                 <h1 className='h1Style'>Echa un vistazo a los nuevos drops</h1>
                 <div className="newDropsHome2">
-                    {productsData.products.slice(0, 3).map((product) => (  // Cambié aquí
+                    {productsData.products.slice(0, 3).map((product) => (
                         <NavLink
                             to={`/product/${product.id}`}
                             key={product.id}
@@ -172,9 +163,6 @@ export const HomePage = () => {
                     ))}
                 </div>
             </section>
-            </article>
-
-
         </main>
     );
 };
